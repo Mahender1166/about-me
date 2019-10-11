f06a45efc163dd30c136aee1266bd05b532d4cf1
Mahender <mahender.lfl95@gmail.com>
2019-10-11 16:19:25 -0500

    entered an invalid test case

--- test/test.js
+++ test/test.js
@@ -6,4 +6,5 @@ QUnit.test('TEST add', assert => {
   assert.equal(calc(1, -1), -1, 'test 3 for one negative values')
   assert.equal(calc(1 , 2) , 2,'test 4 for float values')
   assert.equal(calc(-10, 10), -100, 'test 5 for large numbers')
+  assert.equal(calc('a', 0),NaN,'test 6 for invalid input')
 })
